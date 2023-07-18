1e3cbe23676f39ad45fc03b888761ac2c99014c4
Devluciano77 <lmrottoli301097@gmail.com>
2023-07-18 00:15:13 -0300

    se modifico estilos del navbar, se agrego efecto scroll

--- front/bef/src/components/navbar.jsx
+++ front/bef/src/components/navbar.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, Outlet } from 'react-router-dom'
+import classnames from 'classnames';
 import '../styles/navbar.css'
 import imgLogo from '../images/port 2 be.jpg';    
 
 const Navbar = () =>{
+    const [navbarBg, setNavbarBg] = useState(false);
+
+    const changeNavbarBg = () => {
+      if (window.scrollY >= 50) {
+        setNavbarBg(true);
+      } else {
+        setNavbarBg(false);
+      }
+    };
+  
+    useEffect(() => {
+      window.addEventListener('scroll', changeNavbarBg);
+      return () => {
+        window.removeEventListener('scroll', changeNavbarBg);
+      };
+    }, []);
+
     return(
         <>
         
-                <nav id='navbarContainer' className="navbar fixed-top navbar-expand-lg ">
+                <nav id='navbarContainer'  className={classnames('navbar', { 'navbar-bg-color': navbarBg }, 'fixed-top', 'navbar-expand-lg')}>
                     <div className="container-fluid">
-                        <Link className="navbar-brand text-light" to={"/"}>BE
+                        <Link className="navbar-brand text-light letrasNav" to={"/"}>BE
                             {/* <img src={imgLogo} alt="Bootstrap" width="30" height="24" /> */}
                         </Link>
                         <button className="navbar-toggler  text-bg-secondary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation" >
@@ -18,22 +36,22 @@ const Navbar = () =>{
                         <div className="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
                         <ul id='navbutton' className="navbar-nav">
                             <li className="nav-item">
-                            <Link className="nav-link text-light" aria-current="page" to={"/"}>HOME</Link>
+                            <Link className="nav-link text-light letrasNav" aria-current="page" to={"/"}>HOME</Link>
                             </li>
                             <li className="nav-item">
-                            <Link className="nav-link text-light" to={`/bandas`}>BANDAS</Link>
+                            <Link className="nav-link text-light letrasNav" to={`/bandas`}>BANDAS</Link>
                             </li>
                             <li className="nav-item">
-                            <Link className="nav-link text-light" to={`/evento`}>SOBRE EL EVENTO</Link>
+                            <Link className="nav-link text-light letrasNav" to={`/evento`}>SOBRE EL EVENTO</Link>
                             </li>
                             <li className="nav-item">
-                            <Link className="nav-link text-light" to={`/nosotros`}>NOSOTROS</Link>
+                            <Link className="nav-link text-light letrasNav" to={`/nosotros`}>NOSOTROS</Link>
                             </li>
                             <li className="nav-item">
-                            <Link className="nav-link text-light" to={`/postales`}>POSTALES</Link>
+                            <Link className="nav-link text-light letrasNav" to={`/postales`}>POSTALES</Link>
                             </li>
                             <li className="nav-item">
-                            <Link className="nav-link text-light" to={`/tienda`}>TIENDA</Link>
+                            <Link className="nav-link text-light letrasNav" to={`/tienda`}>TIENDA</Link>
                             </li>
                         </ul>
                         </div>
