c9621ba2861e06f3707ab8db2e14712fd55de6b9
Devluciano77 <lmrottoli301097@gmail.com>
2023-07-17 23:36:05 -0300

    se modificaron componentes (evento y nosotros)

--- front/bef/src/components/Evento.jsx
+++ front/bef/src/components/Evento.jsx
@@ -20,24 +20,28 @@ const Evento = () => {
 
                 <div id="container-cards" className="container-fluid text-center" >
 
-                    <div className="container container-fluid">
+                    <div id='transparentCard' className="container container-fluid">
                         <div className="col">
-                            <div className="card h-100">
-                                <div className="inner"> 
+                            <div className="card h-100 bg-transparent">
+                                {/* <div className="inner"> 
                                     <img src={Foto2} className="card-img-top" alt="..."/>
+                                </div> */}
+                                <div className="card-body">
+                                    {/* <h5 className="card-title">Nerium</h5> */}
+                                    <p className="card-text text-light">El 3, 4 y 5 de Noviembre Hell  Heaven Open Air regresa a los verdes campos de Foro Pegaso <br />
+                                    <br />
+                                    Para esta nueva edición se tiene preparado uno de los line ups más ambiciosos en la historia de este festival, HH 23 inicia un viaje excepcional a través de los tiempos reuniendo grandes leyendas con las nuevas propuestas más sobresaliente del rock y metal actual, sumándole una gran variedad de atracciones, sin duda te harán vivir una experiencia distinta y memorable. </p>
                                 </div>
-                            <div className="card-body">
-                                {/* <h5 className="card-title">Nerium</h5> */}
-                                <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam nostrum praesentium voluptatibus! Magni sed, officiis dolore ullam possimus exercitationem iusto sit pariatur qui in eligendi placeat animi, velit quaerat! Ad.
-                                lo</p>
-                            </div>
-                            <div className="card-footer">
-                            {/* <small className="text-body-secondary">28 de junio</small> */}
-                            </div>
+
+                                <button className="btn btn-outline-success " type="button">Reserva tu anticipada</button>
+
+                                <div className="card-footer">
+                                {/* <small className="text-body-secondary">28 de junio</small> */}
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div id='mapa'>
+                    <div className='container'>
                         <h1 className='text-light justify-content-center' >UBICACION</h1>
                         <GoogleMap />
                     </div>
--- front/bef/src/components/Nosotros.jsx
+++ front/bef/src/components/Nosotros.jsx
@@ -10,8 +10,8 @@ const Nosotros = () => {
       <div className="container-fluid">
         <hr />
         <hr />
-        <h1 className="mt-5">NOSOTROS EN DESARROLLO</h1>
-        <div id='syleCard' className="card mb-3" >
+        <h1 className="mt-5">HISTORIA</h1>
+        <div id='syleCard' className="card mb-3 bg-transparent text-light" >
           <div className="row g-0">
             <div className="col-md-4 h-100">
               <div className='inner' >
@@ -23,8 +23,10 @@ const Nosotros = () => {
               <div className="card-body">
                 <h5 className="card-title">Bien Extremo Fest</h5>
                 <p className="card-text">Organización de eventos de música extrema autogestionada..</p>
-                <p className="card-text"><small className="text-body-secondary">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa a aliquam corrupti ducimus animi eos, officia impedit exercitationem, quod voluptate nam autem fuga. Distinctio quis nesciunt aspernatur recusandae sequi voluptas.
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia, in ea debitis vel, consequatur facilis necessitatibus assumenda tempora maxime commodi quibusdam, sint aperiam! Laboriosam, ipsa. Debitis accusamus velit exercitationem provident.</small></p>
+                <p className="card-text text-light">Hell and Heaven Open Air surge como una iniciativa de empresarios mexicanos que vieron dentro del mercado nacional, una enorme oportunidad de crecimiento para el desarrollo del rock y el metal.
+
+                Hell And Heaven Metal Fest, fue originalmente una inversión de las productoras de espectáculos INFINITY (propiedad de Guerrero) y CEIME (de Castañeda y Alcaraz) quienes apoyados por el patrocinador Grupo Modelo concibieron el primer festival en diciembre de 2010, ya que ambas productoras tenían fechas con los grupos Brujería y Therion, conciertos que inicialmente se presentarían por separado, pero que finalmente decidieron presentarlos juntos y crear un Festival con tres escenarios al aire libre en el recinto conocido como Calle Dos de Zapopan, Jalisco, al que invitaron un gran número de bandas nacionales para hacerlo más atractivo para la escena mexicana. El nombre de Hell And Heaven Metal Fest, surge debido a que los mismos empresarios habían presentado en ese recinto un par de años antes a la banda de los integrantes de Black Sabbath, con Ronnie James “Dio” en las vocales -Heaven And Hell-, y se quedaron impresionados por la sencillez y carisma del legendario cantante, por lo que decidieron titular el festival con el nombre de la banda invertido, Hell And Heaven Metal Fest, haciendo así homenaje al desaparecido cantante, quien falleció de cáncer unos meses antes de la primera edición del festivalLorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa a aliquam corrupti ducimus animi eos, officia impedit exercitationem, quod voluptate nam autem fuga. Distinctio quis nesciunt aspernatur recusandae sequi voluptas.
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia, in ea debitis vel, consequatur facilis necessitatibus assumenda tempora maxime commodi quibusdam, sint aperiam! Laboriosam, ipsa. Debitis accusamus velit exercitationem provident.</p>
               </div>
             </div>
           </div>
